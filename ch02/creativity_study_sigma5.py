c2c2497757d5572f5c0ea25dbd6a3dd0c8abdcc0
John Evans <jevans97@utk.edu>
2019-08-17 14:59:01 -0400

    Add creativity study

--- ch02/creativity_study_sigma5.py
+++ ch02/creativity_study_sigma5.py
@@ -4,36 +4,30 @@ import numpy as np
 import pandas as pd
 import scipy.stats
 
-path = pathlib.Path.home() / 'data' / 'sleuth3' / 'case0201.csv'
-orig = pd.read_csv(path)
+path = pathlib.Path.home() / 'data' / 'sleuth3' / 'case0101.csv'
+odf = pd.read_csv(path)
 
-df = orig.groupby('Year').agg(['count', 'mean', 'std'])
-df.index = ['1: Pre-drought', '2: Post-drought']
+# Treatment effect of 5.
+odf.loc[odf['Treatment'] == 'Intrinsic', 'Score'] -= 5
+
+df = odf.groupby('Treatment').agg(['count', 'mean', 'std'])
 
 # Make the column index look nice.
 df = df.droplevel(0, axis=1)
-columns = {
-    'count': 'n',
-    'mean': 'Average (mm)',
-    'std': 'Sample SD (mm)'
-}
-df.rename(index=str, columns=columns, inplace=True)
-pd.set_option('display.float_format', "{:.4f}".format)
 print(df)
 
-est = (df.loc["2: Post-drought", "Average (mm)"] 
-       - df.loc["1: Pre-drought", "Average (mm)"])
+est = (df.loc["Intrinsic", "mean"] - df.loc["Extrinsic", "mean"])
 
 print('\n')
 print(f'Estimate of µ2 - µ1 is {est:.4f}')
 
 # Compute the pooled SD
 def pooled_sd(df):
-    n1 = df.iloc[0]['n']
-    n2 = df.iloc[1]['n']
+    n1 = df.iloc[0]['count']
+    n2 = df.iloc[1]['count']
 
-    sd1 = df.iloc[0]['Sample SD (mm)']
-    sd2 = df.iloc[1]['Sample SD (mm)']
+    sd1 = df.iloc[0]['std']
+    sd2 = df.iloc[1]['std']
 
     return np.sqrt(((n1 - 1) * sd1 ** 2 + (n2 - 1) * sd2 ** 2) / (n1 + n2 - 2))
 
@@ -45,8 +39,8 @@ print(f"sp = {sp:.4f}")
 def standard_error(df):
     sp = pooled_sd(df)
 
-    n1 = df.iloc[0]['n']
-    n2 = df.iloc[1]['n']
+    n1 = df.iloc[0]['count']
+    n2 = df.iloc[1]['count']
 
     return sp * np.sqrt(1 / n1 + 1 / n2)
 
@@ -54,7 +48,7 @@ se = standard_error(df)
 print('\n')
 print(f"SE = {se:.4f}")
 
-dof = df.iloc[1]['n'] + df.iloc[0]['n'] - 2
+dof = df.iloc[1]['count'] + df.iloc[0]['count'] - 2
 print(f"Degrees of freedom = {dof}")
 
 
@@ -63,8 +57,8 @@ print(f"t(0.975,{dof}) = {t}")
 ci = [est - se * t, est + se * t]
 print(f"95% Confidence interval = [{ci[0]:.4f}, {ci[1]:.4f}]")
 
-a = orig[orig['Year'] == 1976]['Depth']
-b = orig[orig['Year'] == 1978]['Depth']
+a = odf[odf['Treatment'] == 'Intrinsic']['Score']
+b = odf[odf['Treatment'] == 'Extrinsic']['Score']
 tstat, p = scipy.stats.ttest_ind(a, b, equal_var=False)     
 print(f"t statistic is {tstat:.4f}")
 print(f"The two-sided p-value {p:.4f}")
