74ed24f2cbfcf91c6ca0697d012b3cf9f459bb41
John Evans <john.evans8@montana.edu>
2024-02-28 09:22:15 -0700

    Blah

--- ch12/ch1201.py
+++ ch12/ch1201.py
@@ -1,3 +1,6 @@
+# standard library imports
+import itertools
+
 # 3rd party library imports
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,10 +9,16 @@ import scipy.stats
 import seaborn as sns
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
+from statsmodels.stats.anova import anova_lm
+
 
 sns.set()
 pd.options.display.float_format = "{:.3f}".format
 pd.options.display.max_columns = 12
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "Helvetica"
+})
 
 df = pd.read_csv('case1201.csv')
 print(df.head())
@@ -30,9 +39,11 @@ print(model.summary())
 fig, ax = plt.subplots(ncols=2, sharex=True, sharey=True)
 model = smf.ols(formula='SAT ~ logTakers + Rank + Expend', data=df).fit()
 sm.graphics.plot_ccpr(model, 'Expend', ax=ax[0])
+ax[0].set_title(rf"With Alaska: $\beta$, SE = ({model.params['Expend']:.2f}, {model.bse['Expend']:.2f})")
 
 model = smf.ols(formula='SAT ~ logTakers + Rank + Expend', data=df.query('Expend < 40')).fit()
 sm.graphics.plot_ccpr(model, 'Expend', ax=ax[1])
+ax[1].set_title(rf"Without Alaska: $\beta$, SE = ({model.params['Expend']:.2f}, {model.bse['Expend']:.2f})")
 
 
 # partial residual plots
@@ -40,16 +51,76 @@ sm.graphics.plot_ccpr(model, 'Expend', ax=ax[1])
 fig, ax = plt.subplots(ncols=2, sharex=True, sharey=True)
 model = smf.ols(formula='SAT ~ logTakers + Rank + Public', data=df).fit()
 sm.graphics.plot_ccpr(model, 'Public', ax=ax[0])
-ax[0].set_title('With Louisiana')
+ax[0].set_title(rf"With Louisiana: $\beta$, SE = ({model.params['Public']:.2f}, {model.bse['Public']:.2f})")
 ax[0].set_ylabel('Partial Residual')
 ax[0].set_xlabel('')
 
 model = smf.ols(formula='SAT ~ logTakers + Rank + Public', data=df.query('Public > 50')).fit()
 sm.graphics.plot_ccpr(model, 'Public', ax=ax[1])
-ax[1].set_title('Without Louisiana')
+ax[1].set_title(rf"Without Louisiana: $\beta$, SE = ({model.params['Public']:.2f}, {model.bse['Public']:.2f})")
 ax[1].set_ylabel('')
 ax[1].set_xlabel('')
 
 fig.suptitle('CCPR (partial residual plots)')
 fig.supxlabel('Test Taker Percentage in Public Schools')
 fig.tight_layout()
+
+# Forward Variable Selection
+class ForwardSelect(object):
+
+    def __init__(self, df, endog):
+
+        self.endog = endog
+        self.df = df
+
+        self.current_exog = []
+
+        self.candidates = df.columns.to_list()
+        self.candidates.remove(endog)
+
+    def run_forward(self):
+        """
+        Go thru each new model where the candidate variables are added to the
+        new model exog vars.
+        """
+
+        fresults = []
+
+        for var in self.candidates:
+            formula = f"{self.endog} ~ {' + '.join(self.current_exog + [var])}"
+            model = smf.ols(formula=formula, data=self.df).fit()
+            atable = anova_lm(model)
+            fresults.append(atable.loc[var, 'F'])
+
+        s = pd.Series(fresults, index=self.candidates)
+        print(s)
+
+    def select(self, var):
+        """
+        Add a variable to the set list of exog vars, probably because of high
+        f-value.
+        """
+
+        self.candidates.remove(var)
+        self.current_exog.append(var)
+
+    def __str__(self):
+        if len(self.current_exog) == 0:
+            formula = f"{self.endog} ~ 1"
+        else:
+            formula = f"{self.endog} ~ {' + '.join(self.current_exog)}"
+        model = smf.ols(formula=formula, data=self.df).fit()
+        return str(model.summary())
+
+
+data = df.query('Expend < 40').drop(['State', 'Takers'], axis='columns')
+fs = ForwardSelect(data, 'SAT')
+fs.run_forward()
+
+fs.select('logTakers')
+fs.run_forward()
+
+fs.select('Expend')
+fs.run_forward()
+
+print(fs)
